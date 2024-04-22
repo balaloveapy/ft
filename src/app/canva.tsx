7af8b9017878084f2587c8e8cd62fbf563837d34
natan <adaurinega@gmail.com>
2024-04-22 10:43:43 -0300

    a

--- src/app/canva.tsx
+++ src/app/canva.tsx
@@ -1,41 +1,40 @@
 'use client'
 import { useRef, useEffect } from "react";
 export default function Home() {
-  const referencia = useRef<HTMLVideoElement>(null);
-  const referenciaCanva = useRef<HTMLCanvasElement>(null);
+    const referencia = useRef<HTMLVideoElement>(null);
+    const referenciaCanva = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(stream => {
-          if (referencia.current) {
-            referencia.current.srcObject = stream;
-            referencia.current.play();
-          }
-        })
-        .catch(error => {
-          console.log("Error accessing webcam:", error);
-        });
-    }
-  }, []);
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            navigator.mediaDevices.getUserMedia({ video: true })
+                .then(stream => {
+                    if (referencia.current) {
+                        referencia.current.srcObject = stream;
+                        referencia.current.play();
+                    }
+                })
+                .catch(error => {
+                    console.log("Error accessing webcam:", error);
+                });
+        }
+    }, []);
 
-  const takePhoto = () => {
-    if (referencia.current && referenciaCanva.current) {
-      const canvas = referenciaCanva.current.getContext('2d');
-      if (canvas) {
-        canvas.drawImage(referencia.current, 0, 0, referenciaCanva.current.width, referenciaCanva.current.height);
-      }
-    }
-  };
+    const takePhoto = () => {
+        if (referencia.current && referenciaCanva.current) {
+            const canvas = referenciaCanva.current.getContext('2d');
+            if (canvas) {
+                canvas.drawImage(referencia.current, 0, 0, referenciaCanva.current.width, referenciaCanva.current.height);
+            }
+        }
+    };
 
-  return (
-    <div>
-        <div className="absolute bg-black w-full h-full">
-            <button className="bg-slate-300 text-white" onClick={takePhoto}>Take Photo</button>
+    return (
+        <div>
+            <video ref={referencia} autoPlay></video>
+            <div className="absolute bg-black w-full h-full">
+                <button className="bg-slate-300 text-white" onClick={takePhoto}>Take Photo</button>
+                <canvas ref={referenciaCanva} className="w-28 h-28"></canvas>
+            </div>
         </div>
-      <video ref={referencia} autoPlay></video>
-      <canvas ref={referenciaCanva} className="w-28 h-28"></canvas>
-      
-    </div>
-  );
+    );
 }
